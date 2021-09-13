461c47ffdfa37a57700b2314997e87f7fe68f996
Masahiro <89887202+MasaSogami@users.noreply.github.com>
2021-09-13 11:35:14 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,15 +18,15 @@ const app = Vue.createApp({
       */
       correctAnswer: {
         stage1: {
-          q1: 'あああ',
+          q1: 'たからさがし',
         },
         stage2: {
-          q1: 'いいい',
+          q1: '奇数',
           // q2: 'えええ',
           // q3: 'おおお'
         },
         stage3: {
-          q1: 'ううう',
+          q1: '挟み撃ち',
           // q2: 'かかか',
           // q3: 'ききき',
         }
